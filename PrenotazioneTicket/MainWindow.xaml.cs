ee7e8148e159f0ef75b32bd72c14caab5012d6be
AlexBuratta13 <alex,buratta13@gmail.com>
2021-02-02 12:31:44 +0100

    aggiunta classe Cliente

--- PrenotazioneTicket/MainWindow.xaml.cs
+++ PrenotazioneTicket/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LibraryCliente;
 
 namespace PrenotazioneTicket
 {
@@ -23,6 +24,51 @@ namespace PrenotazioneTicket
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnAggiungiCliente_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string nome;
+                string cognome;
+
+                if (txt_Nome.Text != "")
+                {
+                    nome = txt_Nome.Text;
+                }
+                else throw new Exception("Inserisci un nome!");
+
+                if (txt_Cognome.Text != "")
+                {
+                    cognome = txt_Cognome.Text;
+                }
+                else throw new Exception("Inserisci un cognome!");
+
+                Cliente cliente = new Cliente(nome, cognome);
+
+                cliente.SetCellulare(txt_Cellulare.Text);
+                if (btnM.IsChecked == true)
+                {
+                    cliente.SetSesso(true);
+                }
+                else
+                {
+                    cliente.SetSesso(false);
+                }
+
+                cmbClienti.Items.Add(cliente.Stampa());
+                cmbClienti1.Items.Add(cliente.Stampa());
+
+                txt_Nome.Clear();
+                txt_Cognome.Clear();
+                txt_Cellulare.Clear();
+                btnM.IsChecked = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
